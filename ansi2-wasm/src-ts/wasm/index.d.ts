5eabad41f7544ac4fa78fe28e24f311475a09544
ahaoboy <504595380@qq.com>
2024-10-17 16:30:21 +0800

    add lefthook

--- ansi2-wasm/src-ts/wasm/index.d.ts
+++ ansi2-wasm/src-ts/wasm/index.d.ts
@@ -1,40 +1,37 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
-* @param {string} s
-* @param {Theme} theme
-* @param {number | undefined} [width]
-* @param {string | undefined} [font]
-* @param {Mode | undefined} [mode]
-* @returns {string}
-*/
+ * @param {string} s
+ * @param {Theme} theme
+ * @param {number | undefined} [width]
+ * @param {string | undefined} [font]
+ * @param {Mode | undefined} [mode]
+ * @returns {string}
+ */
 export function to_svg(s: string, theme: Theme, width?: number, font?: string, mode?: Mode): string;
 /**
-* @param {string} s
-* @param {Theme} theme
-* @param {number | undefined} [width]
-* @param {string | undefined} [font]
-* @param {Mode | undefined} [mode]
-* @returns {string}
-*/
+ * @param {string} s
+ * @param {Theme} theme
+ * @param {number | undefined} [width]
+ * @param {string | undefined} [font]
+ * @param {Mode | undefined} [mode]
+ * @returns {string}
+ */
 export function to_html(s: string, theme: Theme, width?: number, font?: string, mode?: Mode): string;
 /**
-* @param {string} s
-* @param {number | undefined} [width]
-* @returns {string}
-*/
+ * @param {string} s
+ * @param {number | undefined} [width]
+ * @returns {string}
+ */
 export function to_text(s: string, width?: number): string;
-/**
-*/
 export enum Mode {
   Dark = 0,
   Light = 1,
 }
-/**
-*/
 export enum Theme {
   Vscode = 0,
   Ubuntu = 1,
   Vga = 2,
+  Xterm = 3,
 }
 
