0e6a3bcc08fa42b7a78d4351e66a6a650c0e1e90
ahaoboy <504595380@qq.com>
2024-10-27 18:15:28 +0800

    wasm: add to_ans

--- assets/update.sh
+++ assets/update.sh
@@ -3,6 +3,8 @@
 for i in win11 vitest 8bit-color 24bit-color nu-ls ansi-default colortable hyperlink-demo
 do
   cat "$i.ans" | node ../ansi2-wasm/bin/cli.js -c > "$i.svg"
+  cat "$i.ans" | node ../ansi2-wasm/bin/cli.js -c -f=ans > "$i.min.ans"
+
   cat "$i.ans" | node ../ansi2-wasm/bin/cli.js --mode=light -c > "$i-light.svg"
   cat "$i.ans" | node ../ansi2-wasm/bin/cli.js --mode=dark -c > "$i-dark.svg"
 
